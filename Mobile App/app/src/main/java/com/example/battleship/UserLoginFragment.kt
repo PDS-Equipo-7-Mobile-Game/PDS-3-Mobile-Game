e52732cee770c5764e50d922c6d387cc170b5ed4
Matiasba8 <mibustos2@miuandes.cl>
2022-10-05 11:27:33 -0300

    fix(rename user_login to UserLoginFragment)

--- Mobile App/app/src/main/java/com/example/battleship/UserLoginFragment.kt
+++ Mobile App/app/src/main/java/com/example/battleship/UserLoginFragment.kt
@@ -6,14 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class user_login : Fragment() {
 
+class UserLoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_user_login, container, false)
     }
 }
