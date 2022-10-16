76c326fd01108522793bee563a98eec44377b72c
gudtogo <earias_1995@hotmail.com>
2022-10-15 21:40:34 -0300

    Friend Listview, Friend class dummy y adapter

--- Mobile App/app/src/main/java/com/example/battleship/ui/view/users/UserLoginFragment.kt
+++ Mobile App/app/src/main/java/com/example/battleship/ui/view/users/UserLoginFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.navigation.Navigation
 import com.example.battleship.R
+import com.example.battleship.data.models.Friend
+import com.example.battleship.databinding.ActivityMainBinding
 
 
 class UserLoginFragment : Fragment() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var friendArrayList : ArrayList<Friend>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,6 +29,12 @@ class UserLoginFragment : Fragment() {
 
         new_account_textview?.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_userLoginFragment_to_createNewAccountFragment)
+        }
+
+        var loginButton = view.findViewById<Button>(R.id.button)
+
+        loginButton?.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_userLoginFragment_to_FriendListFragment)
         }
 
         // Inflate the layout for this fragment
