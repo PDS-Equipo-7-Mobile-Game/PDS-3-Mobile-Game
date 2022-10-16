95273b8006e1f8e38b0a745babd9ffaee129a778
Matías Bustos <62266396+Matiasba8@users.noreply.github.com>
2022-10-15 22:14:11 -0300

    Delete BattleShipAPI directory

--- BattleShipAPI/app/models/player.rb
+++
@@ -1,6 +0,0 @@
-class Player < ApplicationRecord
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :email, presence: true
-  validates :email, uniqueness: true
-  validates :name, presence: true
-end
